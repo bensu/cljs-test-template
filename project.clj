4b054f880d9d0d7b2b881917bef81f389bccece5
Sebastian Bensusan <sbensu@gmail.com>
2015-04-20 20:16:49 +0200

    Whitespace

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/bensu/cljs-test-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm { :name "git"
-         :url "https://github.com/bensu/cljs-test-template"}
+  :scm {:name "git"
+        :url "https://github.com/bensu/cljs-test-template"}
   :eval-in-leiningen true)

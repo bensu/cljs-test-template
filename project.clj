8d53dd2c3bf59016eb0099c48987b65c83bd7100
Sebastian Bensusan <sbensu@gmail.com>
2015-04-20 21:47:08 +0200

    Bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-test/lein-template "0.2.5-SNAPSHOT"
+(defproject cljs-test/lein-template "0.2.5"
   :description "A Leinigen template to test ClojureScript apps"
   :url "https://github.com/bensu/cljs-test-template"
   :license {:name "Eclipse Public License"
